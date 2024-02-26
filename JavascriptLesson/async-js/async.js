d8e04b92bb7da6cf56238c95a289bb2f98cfbbba
Bùi Quốc Huy <buiquochuy2001@gmail.com>
2024-02-26 20:34:39 +0700

    update bài học ở lớp

--- JavascriptLesson/async-js/async.js
+++ JavascriptLesson/async-js/async.js
@@ -36,53 +36,55 @@ function CallBackAsync() {
 //promise thuộc es7
 
 // EXAMPLE 1
-// const myPromise = new Promise((resolve, reject) => {});
-// console.log(typeof myPromise);
-// console.log(myPromise);
+const myPromise = new Promise((resolve, reject) => {});
+console.log("----------[BÀI TẬP 1]--------------");
+console.log(typeof myPromise);
+console.log(myPromise);
+console.log("-----------------------------------");
 
 //EXAMPLE 2
-// const newPromise = new Promise((resolve, reject) => {
-//   // xử lý bất đồng bộ ở đây
-//   // fullfiled - xử lý bất đồng thì chạy resolve
-//   //   resolve();
-//   //   rejected - xử lý bất đồng bộ không thành công thì chạy reject
-//   reject("lỗi hiển thị");
-// });
+const newPromise = new Promise((resolve, reject) => {
+  // xử lý bất đồng bộ ở đây
+  // fullfiled - xử lý bất đồng thì chạy resolve
+  //   resolve();
+  //   rejected - xử lý bất đồng bộ không thành công thì chạy reject
+  reject("lỗi hiển thị");
+});
 
-// const FullFilledCallBack = (data) => {
-//   console.log("xử lý bất đồng bộ thành công: ", data);
-// };
-// const ReJectCallBack = (err) => {
-//   console.log("Xử lý bất đồng bộ không thành công: ", err);
-// };
-// const SuccessAfterCallBack = () => {
-//   console.log("Chắc chắn khi xử lý bất đồng bộ thì ở đây sẽ chạy");
-// };
+const FullFilledCallBack = (data) => {
+  console.log("[BÀI TẬP 2] xử lý bất đồng bộ thành công: ", data);
+};
+const ReJectCallBack = (err) => {
+  console.log("[BÀI TẬP 2] Xử lý bất đồng bộ không thành công: ", err);
+};
+const SuccessAfterCallBack = () => {
+  //   console.log("[BÀI TẬP 2] Chắc chắn khi xử lý bất đồng bộ thì ở đây sẽ chạy");
+};
 
-// newPromise
-//   .then((data) => FullFilledCallBack(data))
-//   .catch((err) => ReJectCallBack(err))
-//   .finally(SuccessAfterCallBack);
+newPromise
+  .then((data) => FullFilledCallBack(data))
+  .catch((err) => ReJectCallBack(err))
+  .finally(SuccessAfterCallBack);
 
 // EXAMPLE 3 - BASE PROMISE
 const Ex3Promise = new Promise((resolve, reject) => {
-  const isSuccessFul = false;
+  const isSuccessFul = true; // ví dụ để test promise
 
   if (isSuccessFul) {
     // resolve(); // xử lý thành công undefined
     resolve({ id: 1, user: "Anonymouse" }); // xử lý thành công nhưng có object
   } else {
     // reject(); // xử lý thất bại undefined
-    reject("Thất bại"); // xử lý thất bại nhưng báo lỗi
+    reject("[BÀI TẬP 3] Thất bại"); // xử lý thất bại nhưng báo lỗi
   }
 });
 
 Ex3Promise.then((data) => {
   // xu ly khi nhan data
-  console.log("xử lý thành công: ", data);
+  console.log("[BÀI TẬP 3] Xử lý thành công: ", data);
 })
   .catch((err) => {
     //xy ly khi xay ra loi
-    console.log("Xử lý thất bại: ", err);
+    console.log("[BÀI TẬP 3] Xử lý thất bại: ", err);
   })
   .finally(() => {});
