09f564112aad36ac701b68c149eb145d731f4e43
Bùi Quốc Huy <buiquochuy2001@gmail.com>
2024-04-08 22:03:40 +0700

    update: lesson 3

--- reactjs/lesson3/src/App.jsx
+++ reactjs/lesson3/src/App.jsx
@@ -1,29 +1,83 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import ComponentA from './components/ComponentA';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  function helloWord() {
+    console.log('Hello');
+  }
 
+  const styleObject = {
+    border: '1px solid blue',
+    padding: '20px 20px',
+    fontSize: '20px',
+  };
+
+  const user = {
+    firstName: 'Bui Quoc',
+    lastName: 'Huy',
+    showFullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+
+  const firstName = 'Bui Quoc';
+  const lastName = 'Huy';
+  function getFullName(a, b) {
+    return `${a} ${b}`;
+  }
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello World</h1>
+        <p>My first paragraph</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>DANH SÁCH LỚP HỌC</h2>
+        <ol>
+          <li>
+            Cơm trưa
+            <ul>
+              <li>Cơm chiên hải sản</li>
+              <li>Cơm sườn non nấu cam</li>
+              <li>Cơm canh chua cá lóc</li>
+            </ul>
+          </li>
+          <li>
+            Tráng miệng trái cây
+            <ul>
+              <li>Nho tươi</li>
+              <li>Chuối</li>
+              <li>Mận</li>
+            </ul>
+          </li>
+        </ol>
+
+        <ol>
+          <li>Nguyễn Ngọc Duy</li>
+          <li>Tống Quang Anhh</li>
+          <li>Hà Văn Tiến</li>
+          <li>Đinh Thu Loan</li>
+          <li>Nguyễn Hải Đăng</li>
+          <li>Nguyễn Tiến Quang</li>
+          <li>Nguyễn Văn Chiến</li>
+        </ol>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <button onClick={helloWord}>Hello World</button>
+      <p style={{ border: '1px solid red', padding: '20px 20px', fontSize: '20px' }}>Hello World</p>
+      <p style={styleObject}>Hello World</p>
+      <p className="borderpurple">Hello World</p>
+      <p>Users: {user.showFullName()}</p>
+      <p></p>
+      <ol>
+        <li>First Name: {firstName}</li>
+        <li>Last Name: {lastName}</li>
+        <li>Full Name: {getFullName(firstName, lastName)}</li>
+      </ol>
+
+      <div>
+        {' '}
+        <ComponentA />
+      </div>
     </>
   );
 }
---
+++ reactjs/lesson3/src/components/ComponentA.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+function ComponentA() {
+  return (
+    <>
+      <div className="card">
+        <h2>Học lập trình có đơn giản không</h2>
+        <img src="https://www.imgacademy.com/sites/default/files/styles/scale_1700w/public/2022-07/img-homepage-meta_0.jpg?itok=LMirU0Ik" alt="" />
+        <h3>Đây là tiêu đề bài viết</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis esse fuga perferendis! Reprehenderit dolor maxime voluptas. Quasi temporibus quo voluptate ullam voluptatem minima molestiae! Accusamus inventore quis fugiat doloribus dolores.</p>
+        <button className="fr-1">Đọc thêm</button>
+      </div>
+    </>
+  );
+}
+
+export default ComponentA;
