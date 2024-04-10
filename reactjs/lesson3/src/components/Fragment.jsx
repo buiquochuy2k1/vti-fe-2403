4424b403a752022c7dc0eed529ca27d59c8f0bc7
Bùi Quốc Huy <buiquochuy2001@gmail.com>
2024-04-10 22:06:23 +0700

    update: lesson7_2

--- reactjs/lesson3/src/components/Fragment.jsx
+++ reactjs/lesson3/src/components/Fragment.jsx
@@ -6,8 +6,14 @@ const Fragment = () => {
     setCount(count + 1);
   }
   function handleDecrease() {
-    setCount(count - 1);
+    setCount((prevState) => prevState - 1);
   }
+
+  const [inputValue, setInputValue] = useState('');
+  const handleSubmit = () => {
+    alert(inputValue);
+    // You can perform additional actions here, like sending the input value to a server
+  };
   return (
     <>
       <p>Some text</p>
@@ -16,9 +22,19 @@ const Fragment = () => {
       <h2>Another heading</h2>
       <p>Even more text</p>
       <p>-------------------------------------------------------------</p>
-      <p>Count: {count}</p>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleDecrease}>Decrease</button>
+      <p className={count % 2 == 0 ? 'odd' : 'even'}>Count: {count}</p>
+      <button disabled={count >= 10} onClick={handleIncrease}>
+        Increase
+      </button>
+      <button disabled={count <= 0} onClick={handleDecrease}>
+        Decrease
+      </button>
+      <p>-------------------------------------------------------------</p>
+      <div>
+        <p>Name: {inputValue}</p>
+        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Nhập gì đó..." />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
     </>
   );
 };
